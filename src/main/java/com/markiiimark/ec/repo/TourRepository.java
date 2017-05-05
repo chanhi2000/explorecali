d46464923d554ff2138101f37aa003f6bd1be89d
chanhi2000 <chanhi2000@gmail.com>
2017-05-05 18:54:11 +0900

    v0.0.4: changed TourRepository Interface

--- src/main/java/com/markiiimark/ec/repo/TourRepository.java
+++ src/main/java/com/markiiimark/ec/repo/TourRepository.java
@@ -1,9 +1,13 @@
 package com.markiiimark.ec.repo;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.markiiimark.ec.domain.Tour;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
 
+
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+	List<Tour> findByTourPackageCode(String code);
 }
