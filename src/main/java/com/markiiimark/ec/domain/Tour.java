eee058f45053d476326ec0964ae0cdfd94026ceb
chanhi2000 <chanhi2000@gmail.com>
2017-05-05 18:48:18 +0900

    v0.0.4: importing the entire persistence library

--- src/main/java/com/markiiimark/ec/domain/Tour.java
+++ src/main/java/com/markiiimark/ec/domain/Tour.java
@@ -3,11 +3,7 @@ package com.markiiimark.ec.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 public class Tour implements Serializable{
